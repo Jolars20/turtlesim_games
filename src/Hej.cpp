fa848f82749490b200fa618ab819c6d4416856f5
J.B. Larsen <jolars20@student.aau.dk>
2020-10-20 11:04:31 +0200

    Hej fra karantæne

---
+++ src/Hej.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+int main( int argc, char const *argv[])
+{
+    int clock = 0;
+    std::cout << " Hvad er klokken? \n" << "p.s. brug ikke colon. \n";
+    std::cin >> clock;
+    std::cout << " Forståeligt. Hav en god dag.\n";
+return 0;
+}
